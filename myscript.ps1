6c25fb584b923cf217f81fb36b595d7c13264f5a
Sandesh <bhusalsandesh0@gmail.com>
2024-01-16 23:27:24 +0545

    helloworld script

--- myscript.ps1
+++ myscript.ps1
@@ -1,14 +1,16 @@
 function Get-HelloWorld {
+    Clear-Host
     Write-Output "Hello, World!"
 }
 
 function Get-UserInput {
+    Clear-Host
     $input = Read-Host "Enter something:"
+    Clear-Host
     Write-Output "You entered: $input"
 }
 
 function Show-Menu {
-    Clear-Host
     Write-Host "1. Get Hello World"
     Write-Host "2. Get User Input"
     Write-Host "Q. Quit"
@@ -16,18 +18,21 @@ function Show-Menu {
 
 while ($true) {
     Show-Menu
-    $choice = Read-Host "Enter your choice"
+    $choice = Read-Host "`nEnter your choice"
 
     switch ($choice.ToUpper()) {
         '1' { 
             Get-HelloWorld
-            Read-Host "Press Enter to continue"
+            Write-Host "`nPress Enter to continue"
+            Read-Host
         }
         '2' { 
             Get-UserInput
-            Read-Host "Press Enter to continue"
+            Write-Host "`nPress Enter to continue"
+            Read-Host
         }
-        'Q' { break }
+        'Q' { Exit }
         default { Write-Host "Invalid choice. Try again." }
     }
+    Clear-Host
 }
